e4ea25e6d3fcd5dbd0f667b68013eeb4583be7e4
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2023-08-28 00:44:45 +0300

    Added functional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,35 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
+  componentDidMount() {
+    let contacts = localStorage.getItem('contacts');
+    if (!contacts) {
+      //Added for test!!!!
+      let contacts = [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ];
+      this.setState({ contacts: contacts });
+    } else {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleAddContact = contact => {
     this.setState(prev => {
-      return { contacts: prev.contacts.concat(contact), name: '', number: '' };
+      return { contacts: [...prev.contacts, contact], name: '', number: '' };
     });
   };
 
